7d77f6250715cc18234d49dcf25dca568e40d4d6
yesy <yesy9379@163.com>
2023-07-04 14:27:20 +0800

    hover后右滚动

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    SwitchDemo
+    <Switch />
   </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+  components: { Switch },
+  setup() {
+
+  },
+}
+</script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,30 @@
+<template>
+  <button><span></span></button>
+</template>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: rgb(55, 79, 215);
+  border-radius: $h/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: #fff;
+  border-radius: $h2/2;
+}
+
+button:hover>span {
+  left: calc(100% - #{$h2} - 2px);
+}
+</style>
