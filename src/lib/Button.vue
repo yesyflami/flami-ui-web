743ff621fabdfefa41c632c9c4d8378284b16844
yesy <yesy9379@163.com>
2023-07-29 00:21:56 +0800

    支持loading加载

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="flami-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="flami-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -23,6 +24,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
   setup(props) {
     const { theme, size, level } = props;
@@ -190,5 +195,28 @@ $grey: grey;
       color: $grey;
     }
   }
+
+  >.flami-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: flami-spin 1s infinite linear;
+  }
+}
+
+@keyframes flami-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+
+  100% {
+    transform: rotate(360deg)
+  }
+
 }
 </style>
