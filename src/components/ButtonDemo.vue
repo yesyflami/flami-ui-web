7138ee6bfbcc86777c2947b68f619d5c98757a25
yesy <yesy9379@163.com>
2023-07-28 13:37:02 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,13 +2,21 @@
   <div>
     ButtonDemo
     <div>
-      <Button>Hello<Button />
+      <Button @click="onClick" @focus="onClick" @mouseover="onClick">Hello</Button>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
-  components: { Button }
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('hi')
+    }
+    return {
+      onClick
+    }
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -3,3 +3,13 @@
     <slot />
   </button>
 </template>
+<script lang="ts">
+export default {
+  // 默认所有属性都可以绑定到根元素，使用inheritAttrs: false可以取消默认的绑定
+  inheritAttrs: false,
+  props: {},
+  setup(props, ctx) {
+  },
+}
+</script>
+<style lang="scss" scoped></style>
