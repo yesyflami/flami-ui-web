6627da5281851016874eed0b8233d74c17440b46
yesy <yesy9379@163.com>
2023-07-28 13:00:52 +0800

    创建Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     ButtonDemo
+    <div>
+      <Button>Hello<Button />
+    </div>
   </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button }
+}
+</script>
