d689942d86f44bfdefcd79b48f52b9bd6e4b9be3
yesy <yesy9379@163.com>
2023-07-28 23:03:18 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -4,7 +4,7 @@
   </button>
 </template>
 <script lang="ts">
-import { computed } from 'vue';
+import { computed } from "vue";
 export default {
   props: {
     theme: {
@@ -14,26 +14,32 @@ export default {
     size: {
       type: String,
       default: "normal",
-    }
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
-  setup(props, ctx) {
-    const { theme, size } = props
+  setup(props) {
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`flami-theme-${theme}`]: theme,
         [`flami-size-${size}`]: size,
-      }
-    })
-    return { classes }
+        [`flami-level-${level}`]: level,
+      };
+    });
+    return { classes };
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 
 .flami-button {
   box-sizing: border-box;
@@ -49,6 +55,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   &+& {
     margin-left: 8px;
@@ -87,24 +94,76 @@ $radius: 4px;
     &:hover,
     &:focus {
       background: darken(white, 5%);
-      ;
     }
   }
 
+  &.flami-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.flami-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
   &.flami-theme-button {
-    &.flami-size-large {
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px
+    &.flami-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
 
+    &.flami-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
 
-    &.flami-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.flami-theme-link {
+    &.flami-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.flami-theme-text {
+    &.flami-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
     }
 
+    &.flami-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
