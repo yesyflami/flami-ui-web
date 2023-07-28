6e51d9b067c9ee432599cd2b7868f6e08f5bf0f3
yesy <yesy9379@163.com>
2023-07-28 16:25:03 +0800

    size css 2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,27 @@
 <template>
+  <div>ButtonDemo</div>
+  <h1>示例1</h1>
   <div>
-    ButtonDemo
-    <div>
-      <Button>Hello</Button>
-      <Button theme="button">Hello</Button>
-      <Button theme="link">Hello</Button>
-      <Button theme="text">Hello</Button>
-    </div>
+    <Button>Hello</Button>
+    <Button theme="button">Hello</Button>
+    <Button theme="link">Hello</Button>
+    <Button theme="text">Hello</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="large">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+  <div>
+    <Button theme="link" size="large">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small" theme="link">小小小</Button>
+  </div>
+  <div>
+    <Button size="large" theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small" theme="text">小小小</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,30 @@
 <template>
-  <button class="flami-button" :class="{ [`theme-${theme}`]: theme }">
+  <button class="flami-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from 'vue';
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    }
+  },
+  setup(props, ctx) {
+    const { theme, size } = props
+    const classes = computed(() => {
+      return {
+        [`flami-theme-${theme}`]: theme,
+        [`flami-size-${size}`]: size,
+      }
+    })
+    return { classes }
   },
 };
 </script>
@@ -51,6 +66,45 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.flami-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.flami-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+      ;
+    }
+  }
+
+  &.flami-theme-button {
+    &.flami-size-large {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+
+
+    &.flami-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+
   }
 }
 </style>
