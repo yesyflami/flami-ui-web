3a33958a3900ea15b371eae1e96a690d495c808b
yesy <yesy9379@163.com>
2024-03-03 03:03:36 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Tabs示例
   </div>
   <h1>示例一</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -11,10 +11,14 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import { ref } from 'vue';
 export default {
   components: {
     Tabs, Tab
   },
-  // return {}
+  setup(props, ctx) {
+    const x = ref('导航1')
+    return { x }
+  },
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="flami-tabs">
-    <div class="flami-tabs-nav">
-      <div class="flami-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{ t }}</div>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t, index) in titles" :ref="el => { if (el) navItems[index] = el }"
+        @click="select(t)" :class="{ selected: t === selected }" :key="index">{{ t }}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
-    <div class="flami-tabs-content">
-      <component class="flami-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" :class="{ selected: c.props.title === selected }"
+        v-for="(c, idx) in defaults" :is="c" :key="idx" />
+
     </div>
   </div>
 </template>
+  
 <script lang="ts">
 import Tab from './Tab.vue'
+import {
+  computed, onMounted, onUpdated, ref
+} from 'vue'
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -18,12 +31,43 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null) // 移动条
+    const container = ref<HTMLDivElement>(null)
+
+    const x = () => {// 动态获取移动条条的宽度
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const { width } = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      // 动态设置移动条的位置
+      const { left: left1 } = container.value.getBoundingClientRect()
+      const { left: left2 } = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    // onMounted只在第一次渲染执行
+    onMounted(x)
+    onUpdated(x)
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container
     }
   }
 }
@@ -34,11 +78,12 @@ $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
 
-.flami-tabs {
+.gulu-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -53,10 +98,28 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms
+    }
   }
 
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
