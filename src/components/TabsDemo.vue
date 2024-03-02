28fe1da520e26fe6fc16beb5abc4bfbd82e23810
yesy <yesy9379@163.com>
2024-03-03 00:01:47 +0800

    tab css完善

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -4,8 +4,8 @@
   </div>
   <h1>示例一</h1>
   <Tabs>
-    <Tab>内容1</Tab>
-    <Tab>内容2</Tab>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 <script lang="ts">
