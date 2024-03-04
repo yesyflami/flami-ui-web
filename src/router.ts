c049a5289d733459586d4cac5b4809ae35e5814c
yesy <yesy9379@163.com>
2024-03-04 20:18:23 +0800

    抽离用户拷贝的代码

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,36 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
-import { createWebHistory, createRouter } from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStart from './views/GetStart.vue'
-import Install from './views/Install.vue'
-const history = createWebHistory()
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
+const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
-  history,
+  history: history,
   routes: [
-    { path: '/', component: Home },
+    { path: "/", component: Home },
     {
-      path: '/doc', component: Doc,
+      path: "/doc",
+      component: Doc,
       children: [
-        { path: '', component: DocDemo },
-        { path: 'intro', component: Intro },
-        { path: 'install', component: Install },
-        { path: 'get-started', component: GetStart },
-        { path: 'switch', component: SwitchDemo },
-        { path: 'button', component: ButtonDemo },
-        { path: 'dialog', component: DialogDemo },
-        { path: 'tabs', component: TabsDemo }
-      ]
-    }
-  ]
-})
+        { path: "", component: DocDemo },
+        { path: "intro", component: md('intro') },
+        { path: "get-started", component: md('get-started') },
+        { path: "install", component: md('install') },
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
+      ],
+    },
+  ],
+});
+router.afterEach(() => {
+  console.log("路由切换了");
+});
